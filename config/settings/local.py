45e19b069e21c158da9665a8f74a1a6db07131eb
Bakhrom Achilov <mailbak36@gmail.com>
2025-09-27 23:52:08 +0500

    settings changes

---
+++ config/settings/local.py
@@ -0,0 +1,8 @@
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-au!%!#s64^8vtn3gnyq$el=@640v63fum=#q!c@v_&5dm()ew7"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
