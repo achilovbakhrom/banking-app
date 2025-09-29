029b505700a105c62920ac00eff981f23e4797f9
Bakhrom Achilov <mailbak36@gmail.com>
2025-09-29 23:45:48 +0500

    docker compose changes

--- docker/local/django/entrypoint.sh
+++ docker/local/django/entrypoint.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 set -o errexit
 
@@ -18,18 +18,18 @@ start = time.time()
 while True:
     try:
         psycopg2.connect(
-            dbname=${POSTGRES_DB},
-            user=${POSTGRES_USER},
-            password=${POSTGRES_PASSWORD},
-            host=${POSTGRES_HOST},
-            port=${POSTGRES_PORT},
+            dbname="${POSTGRES_DB}",
+            user="${POSTGRES_USER}",
+            password="${POSTGRES_PASSWORD}",
+            host="${POSTGRES_HOST}",
+            port="${POSTGRES_PORT}",
         )
         break
     except psycopg2.OperationalError as error:
-        system.stderr.write("Waiting for PostgreSQL to become available... \n")
+        sys.stderr.write("Waiting for PostgreSQL to become available... \n")
         if time.time() - start > suggest_unrecoverable_after:
-            system.stderr.write("This is taking longer than expected. The following exception may be indiciative of an unrecoverable error: '{}'\n".format(error))
-            time.sleep(3)
+            sys.stderr.write("This is taking longer than expected. The following exception may be indicative of an unrecoverable error: '{}'\n".format(error))
+        time.sleep(3)
 
 END
 
